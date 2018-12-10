9781de4ac3d45dc355dfbf84dbf3c848adae693a
alexandre.carneiro <alexandre.carneiro@fulllab.com.br>
2018-12-09 23:35:08 -0200

    Devise User and Episode/Movie/Season with relationships

---
+++ db/migrate/20181210011710_create_seasons.rb
@@ -0,0 +1,10 @@
+class CreateSeasons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :seasons do |t|
+      t.string :title
+      t.text :plot
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/seasons.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :season do
+    title { "MyString" }
+    plot { "MyText" }
+  end
+end
