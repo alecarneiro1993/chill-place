8e9811f68388dd71ba22bcf79ec55382c9b3acc6
alexandre.carneiro <alexandre.carneiro@fulllab.com.br>
2018-12-13 00:29:34 -0200

    Added validation for repetitive purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,11 +2,16 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase = Purchase.new(purchase_params)
-    if @purchase.save
-      flash[:success] = 'Item purchased'
-      redirect_to :library
+    if !has_been_bought?
+      if @purchase.save
+        flash[:success] = 'Item purchased'
+        redirect_to :library
+      else
+        flash[:alert] = 'Something went wrong with this purchase'
+        redirect_back(fallback_location: root_path)
+      end
     else
-      flash[:alert] = 'Something went wrong with this purchase'
+      flash[:alert] = 'This item has been bought already. Check your library'
       redirect_back(fallback_location: root_path)
     end
   end
@@ -15,6 +20,14 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase).permit(:purchasable_id, :purchasable_type, :user_id, :quality)
+  end
+
+  def has_been_bought?
+    Purchase.where(
+      purchasable_id: @purchase.purchasable_id,
+      user: current_user,
+      quality: @purchase.quality,
+      created_at: 3.days.ago..DateTime::Infinity.new).any?
   end
 
 end
